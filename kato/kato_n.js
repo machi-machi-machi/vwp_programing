bad579cc70a4ec43e87e1fd40908507418fc6582
kurosaki <takuma.k20011016@gmail.com>
2025-12-30 04:52:33 +0900

    加藤かわいそうだから機能追加

--- kato/kato_n.js
+++ kato/kato_n.js
@@ -34,3 +34,23 @@ new Chart(ctx, {
     }
   }
 });
+
+
+// ===== スライドショー =====
+const images = [
+  "../img/KATO/PXL_20231231_044545736.jpg",
+  "../img/KATO/96497_0.jpg",
+  "../img/KATO/96498_0.jpg",
+  "../img/KATO/96499_0.jpg",
+  "../img/KATO/96500_0.jpg",
+  "../img/KATO/96501_0.jpg",
+  "../img/KATO/IMG00160.jpg"
+];
+
+let currentIndex = 0;
+const slideImg = document.getElementById("slideshow");
+
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % images.length;
+  slideImg.src = images[currentIndex];
+}, 4000); // 4秒ごとに切り替え
